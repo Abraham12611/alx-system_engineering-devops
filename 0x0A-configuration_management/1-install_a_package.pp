3237b8c80bda13ee4fdf883a16dc97bb5befc0b6
Abraham12611 <abraham.dahunsi@gmail.com>
2023-05-13 06:28:21 -0700

    Flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,22 @@
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# install_flask.pp
+
+# Install pip3
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Install Flask 2.1.0 using pip3
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => '/usr/bin',
+  require => Package['python3-pip'],
+}
+
+# Run puppet-lint
+# Note: Make sure puppet-lint is installed
+exec { 'puppet_lint':
+  command => 'puppet-lint install_flask.pp',
+  path    => '/usr/bin',
+  require => Exec['install_flask'],
 }
 
