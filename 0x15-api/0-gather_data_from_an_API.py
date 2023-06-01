e8215b2c5ebe19d7331c74523b370275c4d55ff6
Abraham12611 <abraham.dahunsi@gmail.com>
2023-06-01 06:38:08 -0700

    0-gdfaAPI

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,30 +1,24 @@
 #!/usr/bin/python3
-'''A script that gathers data from an API.
 '''
-import re
+Module contains python script for making an api call
+'''
 import requests
 import sys
 
 
-API_URL = 'https://jsonplaceholder.typicode.com'
-'''The API's URL.'''
-
-
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        if re.fullmatch(r'\d+', sys.argv[1]):
-            id = int(sys.argv[1])
-            user_res = requests.get('{}/users/{}'.format(API_URL, id)).json()
-            todos_res = requests.get('{}/todos'.format(API_URL)).json()
-            user_name = user_res.get('name')
-            todos = list(filter(lambda x: x.get('userId') == id, todos_res))
-            todos_done = list(filter(lambda x: x.get('completed'), todos))
-            print(
-                'Employee {} is done with tasks({}/{}):'.format(
-                    user_name,
-                    len(todos_done),
-                    len(todos)
-                )
-            )
-            for todo_done in todos_done:
-                print('\t {}'.format(todo_done.get('title')))
+
+    user_id = sys.argv[1]
+    url = 'https://jsonplaceholder.typicode.com/users/{}/'.format(user_id)
+    todos_url = url + 'todos'
+    user = requests.get(url).json()
+    todos = requests.get(todos_url).json()
+
+    completed_todos = [todo for todo in todos if todo.get('completed') is True]
+
+    print('Employee {} is done with tasks({}/{}):'.format(user.get('name'),
+                                                          len(completed_todos),
+                                                          len(todos)))
+
+    for todo in completed_todos:
+        print('\t {}'.format(todo.get('title')))
